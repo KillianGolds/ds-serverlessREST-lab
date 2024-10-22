ad81a240dabaf299e50c4d9c69d6769ab81ac8d3
KillianGolds <20070585@mail.wit.ie>
2024-10-22 18:38:57 +0100

    API Resource to query cast members.

--- seed/movies.ts
+++ seed/movies.ts
@@ -1,6 +1,22 @@
-import {Movie} from '../shared/types'
+import {Movie, MovieCast} from '../shared/types'
 
 export const movies : Movie[] = [
+  {
+    adult: false,
+    backdrop_path: '/jXJxMcVoEuXzym3vFnjqDW4ifo6.jpg',
+    genre_ids: [28, 14, 32],
+    id: 1234,
+    original_language: 'en',
+    original_title: 'Title 1234',
+    overview: 'Every six years, an ancient order of jiu-jitsu fighters joins forces to battle a vicious race of alien invaders. But when a celebrated war hero goes down in defeat, the fate of the planet and mankind hangs in the balance.',
+    popularity: 2633.943,
+    poster_path: '/somePosterPath.jpg',
+    release_date: '2020-11-20',
+    title: 'Title 1234',
+    video: false,
+    vote_average: 5.9,
+    vote_count: 111
+  },
   {
     adult: false,
     backdrop_path: '/sRLC052ieEzkQs9dEtPMfFxYkej.jpg',
@@ -322,5 +338,32 @@ export const movies : Movie[] = [
     vote_count: 325
   }
 ]
+
+export const movieCasts: MovieCast[] = [
+  {
+    movieId: 1234,
+    actorName: "Joe Bloggs",
+    roleName: "Male Character 1",
+    roleDescription: "description of character 1",
+  },
+  {
+    movieId: 1234,
+    actorName: "Alice Broggs",
+    roleName: "Female Character 1",
+    roleDescription: "description of character 2",
+  },
+  {
+    movieId: 1234,
+    actorName: "Joe Cloggs",
+    roleName: "Male Character 2",
+    roleDescription: "description of character 3",
+  },
+  {
+    movieId: 2345,
+    actorName: "Joe Bloggs",
+    roleName: "Male Character 1",
+    roleDescription: "description of character 3",
+  },
+];
 
 
